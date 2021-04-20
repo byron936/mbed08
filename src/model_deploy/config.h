48ff1d23cbbb62b4e507283c561d3eebef042856
Byron <cm9930914@gmail.com>
2021-04-20 07:22:52 -0700

    finish

--- src/model_deploy/config.h
+++ src/model_deploy/config.h
@@ -4,7 +4,7 @@
 
 // The number of labels (without negative)
 
-#define label_num 2
+#define label_num 3
 
 struct Config
 {
@@ -15,7 +15,7 @@ struct Config
 
     // The number of expected consecutive inferences for each gesture type.
 
-    const int consecutiveInferenceThresholds[label_num] = {20, 10};
+    const int consecutiveInferenceThresholds[label_num] = {20, 10, 10};
 
     const char *output_message[label_num] = {
 
@@ -50,6 +50,9 @@ struct Config
         "   *             \n\r"
 
         "  *              \n\r"
+
+        " * * * * * * * * \n\r",
+        "LINE:\n\r"
 
         " * * * * * * * * \n\r"};
 };
